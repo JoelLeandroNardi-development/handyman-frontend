2b83d6cebc9fe84734a1ba53e0feca26972fc4da
JoelLeandroNardi-development <joelnardi.development@gmail.com>
2026-03-10 16:24:14 +0100

    Some minor improvements on warnings

--- apps/admin-web/src/auth/LoginPage.tsx
+++ apps/admin-web/src/auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ApiClient, login } from "@smart/api";
 import { decodeJwt } from "@smart/core";
